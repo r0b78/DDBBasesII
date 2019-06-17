af0ae373b93dae92f20984565d23df57cab6b960
r0b78 <robert19972008@hotmail.com>
2019-06-16 18:03:39 -0600

    actualizacion

--- AplicacionWebCliente/index.js
+++ AplicacionWebCliente/index.js
@@ -25,7 +25,9 @@ app.post('/login', client.login);
 app.get('/verCompras',client.verCompras)
 
 app.get('/buscarVehiculos',client.buscarVehiculosVentana)
-app.post('/buscarVehiculos',client.verVehiculo)
+app.post('/buscarVehiculos',client.buscarVehiculo)
+app.get('/comprarVehiculo/:id',client.comprarVehiculo)
+
 
 app.get('/verSucursales',client.verSucursales)
 
